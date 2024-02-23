4b8056156265da3564934b9f8902c22a4c3bfc96
BW <zs6@gmail.com>
2024-02-23 13:00:04 +0100

    w1

--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            Console.WriteLine(4+5);
             Console.ReadKey();
         }
     }
